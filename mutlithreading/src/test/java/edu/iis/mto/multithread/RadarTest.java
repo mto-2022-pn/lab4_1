71cf7e0e793d13be15628027800eed023861a8f9
Bartosz <228805@edu.p.lodz.pl>
2022-04-09 16:43:41 +0200

    changed launchPatriotOnceWhenNoticesAScudMissle test that it uses BetterRadar class

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -1,11 +1,14 @@
 package edu.iis.mto.multithread;
 
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.concurrent.Executor;
 
 @ExtendWith(MockitoExtension.class)
 class RadarTest {
@@ -15,10 +18,12 @@ class RadarTest {
 
     @Test
     void launchPatriotOnceWhenNoticesAScudMissle() {
-        Radar radar = new Radar(batteryMock);
+        int rocketCount=1;
+        Executor executor = Runnable::run;
+        BetterRadar betterRadar = new BetterRadar(rocketCount,batteryMock,executor);
         Scud enemyMissle = new Scud();
-        radar.notice(enemyMissle);
-        verify(batteryMock).launchPatriot(enemyMissle);
+        betterRadar.notice(enemyMissle);
+        verify(batteryMock,times(rocketCount)).launchPatriot(enemyMissle);
     }
 
 }
