8fb53b6b8b6a4b709efba817cc41e24d57d82e94
Karol Profic <karolprofic@protonmail.com>
2022-05-19 23:13:45 +0200

    Added BetterRadar

---
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -0,0 +1,23 @@
+package edu.iis.mto.multithread;
+
+import java.util.concurrent.Executor;
+
+public class BetterRadar {
+    private int rocketCount = 10;
+    private PatriotBattery battery;
+    private Executor executor;
+
+    public BetterRadar(int rocketCount, PatriotBattery battery, Executor executor) {
+        this.rocketCount = rocketCount;
+        this.battery = battery;
+        this.executor = executor;
+    }
+
+    public void notice(Scud enemyMissile){
+        executor.execute(()-> {
+            for (int i = 0; i < rocketCount; i++) {
+                battery.launchPatriot(enemyMissile);
+            }
+        });
+    }
+}
